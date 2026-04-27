761381f0c05fec56528e6854b817f40a70f042df
dimideveloper <dimimc232@gmail.com>
2026-04-27 18:21:41 +0200

    fix: disable supabase broadcast to resolve illegal constructor error

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -13,5 +13,10 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABL
     storage: localStorage,
     persistSession: true,
     autoRefreshToken: true,
+  },
+  realtime: {
+    config: {
+      broadcast: { self: false },
+    }
   }
 });
